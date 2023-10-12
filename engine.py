1318ae4460b9f1318c379ccf58941fe25d2c406b
Bolb347 <147656916+Bolb347@users.noreply.github.com>
2023-10-12 10:54:08 -0700

    engine

---
+++ engine.py
@@ -0,0 +1,30 @@
+import pygame as pg
+
+pg.init()
+pg.font.init()
+
+clock = pygame.time.Clock()
+
+class Screen:
+  width = 500
+  height = 500
+  global_screen = pg.display.set_mode((Screen.width, Screen.height))
+
+class DisplayObject:
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+
+  def draw(self, screen):
+    pass
+
+class TextField(DisplayObject):
+  def __init__(text, font='Courier New', color='black', size=12):
+    self.text = text
+    self.font = pygame.font.SysFont(font, size)
+    self.color = color
+    self.size = size
+
+  def draw(self, screen):
+    font_surface = self.font.render(self.text, False, self.color)
+    screen.blit(font_surface, (self.x, self.y))
