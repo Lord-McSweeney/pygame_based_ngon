a8065c2400bf655bd3a025a13254559063146f07
Bolb347 <147656916+Bolb347@users.noreply.github.com>
2023-10-12 11:50:46 -0700

    collision update (box)

--- engine.py
+++ engine.py
@@ -14,9 +14,18 @@ class Screen:
     pygame.display.update()
 
 class DisplayObject:
-  def __init__(self, x, y):
+  renderList = []
+  def render():
+        for i in DisplayObject.renderList:
+            if i.visible:
+                i.draw(Screen.global_screen)
+    
+  def __init__(self, x, y, visible = True):
     self.x = x
     self.y = y
+    self.visible = visible
+
+    renderList.append(self)
 
   def draw(self, screen):
     pass
@@ -31,3 +40,21 @@ class TextField(DisplayObject):
   def draw(self, screen):
     font_surface = self.font.render(self.text, False, self.color)
     screen.blit(font_surface, (self.x, self.y))
+
+class CollisionObject(DisplayObject):
+  collisionList = []
+  def render():
+        for i in CollisionObject.collisionList:
+          for j in CollisionObject.collisionList:
+            if i.visible:
+                i.collision(i, j)
+
+  def __init__(x2, y2):
+    self.x2 = x2
+    self.y2 = y2
+
+    collisionList.append(self)
+
+  def collision(self, other_object):
+    if self.x >= other_object.x and self.x2 <= other_object.x2 and self.y >= other_object.y and self.y2 <= other_object.y2:
+      return(self, other_object)
