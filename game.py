c20f829bba3e98bb227ea63c73f82d350b3e2f71
Bolb347 <147656916+Bolb347@users.noreply.github.com>
2023-10-12 18:12:58 -0700

    game: added a collision class in the main game

--- game.py
+++ game.py
@@ -1,5 +1,23 @@
 from engine import *
 import pygame as pg, random as rd, math as mt
 
+screen = Screen()
+
+class Map(CollisionObject):
+  def __init__(self, x, y, x2, y2, color="black", shape="rectangle"):
+    super().__init__(x, y, x2, x2)
+    self.color = color
+    self.shape = shape
+
+  def draw(self, screen):
+    if self.shape = "rectangle":
+      pygame.draw.rect(screen, self.color, pygame.Rect(self.x, self.y, self.x2, self.y2))
+    else:
+      print("shape not supported")
+    
+
 while True:
-  
+  screen.fill("black")
+
+  screen.update()
+  clock.tick(60)
