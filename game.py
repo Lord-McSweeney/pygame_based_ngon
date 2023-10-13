5a656c3fa05afed4b22735c166e2dd93a7c8f146
Bolb347 <147656916+Bolb347@users.noreply.github.com>
2023-10-13 08:18:39 -0700

    game test

--- game.py
+++ game.py
@@ -2,7 +2,7 @@ from engine import *
 import pygame as pg, random as rd, math as mt
 
 class Map(CollisionObject):
-  def __init__(self, x, y, x2, y2, color="black", shape="rectangle"):
+  def __init__(self, x, y, x2, y2, color="gray", shape="rectangle"):
     super().__init__(x, y, x2, x2)
     self.color = color
     self.shape = shape
@@ -13,9 +13,11 @@ class Map(CollisionObject):
     else:
       print("shape not supported")
     
-
+Map(10, 10, 100, 100)
+Map(300, 300, 200, 200)
 while True:
   Screen.screen.fill("black")
-
+  DisplayObject.render()
+  CollisionObject.render()
   pygame.display.update()
   clock.tick(60)
