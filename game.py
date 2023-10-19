3e23f5551cf5331ee33e0bfbe4528eadab35fd69
Bolb347 <147656916+Bolb347@users.noreply.github.com>
2023-10-18 18:38:34 -0700

    game render fix

--- game.py
+++ game.py
@@ -1,22 +1,81 @@
 from engine import *
-import pygame as pg, random as rd, math as mt
+import pygame, random, math, sys
+
+starting= [
+  (
+    (0, 0),
+    (600, 50),
+    1
+  ),
+  (
+    (0, 0),
+    (50, 600),
+    1
+  ),
+  (
+    (0, 550),
+    (600, 600),
+    1
+  )
+]
 
 class Map(CollisionObject):
   def __init__(self, x, y, x2, y2, color="gray", shape="rectangle"):
-    super().__init__(x, y, x2, x2)
+    super().__init__(x, y, x2, y2)
     self.color = color
     self.shape = shape
+    self.width = self.x2 - self.x
+    self.height = self.y2 - self.y
+    if self.width < 0:
+        self.width = self.x - self.x2
+    if self.height < 0:
+        self.height = self.y - self.y2
 
   def draw(self, screen):
     if self.shape == "rectangle":
-      pygame.draw.rect(screen, self.color, pygame.Rect(self.x, self.y, self.x2, self.y2))
+      pygame.draw.rect(screen, self.color, pygame.Rect(self.x, self.y, self.width, self.height))
     else:
       print("shape not supported")
-    
-Map(10, 10, 100, 100)
-Map(300, 300, 200, 200)
+
+def makeMap(mapn = starting, difficulty = 1):
+  for mapItem in mapn:
+    if mapItem[2] == 1: #block
+        Map(mapItem[0][0], mapItem[0][1], mapItem[1][0], mapItem[1][1])
+
+class Player(CollisionObject):
+    def __init__(self, x, y, x2, y2):
+        super().__init__(x, y, x2, y2)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", ((abs(self.x2-self.x))/2+self.x, (abs(self.y2-self.y))/2+self.y), (abs(self.x2-self.x))/2)
+
+    def controls(self):
+        keys = pygame.key.get_pressed()
+       
+        if keys[pygame.K_LEFT]:
+            self.x -= 10
+            self.x2 -= 10
+        if keys[pygame.K_RIGHT]:
+            self.x += 10
+            self.x2 += 10
+        if keys[pygame.K_UP]:
+            self.y -= 10
+            self.y2 -= 10
+        if keys[pygame.K_DOWN]:
+            self.y += 10
+            self.y2 += 10
+       
+
+makeMap()
+player = Player(100, 100, 200, 200)
+
 while True:
-  Screen.screen.fill("black")
-  DisplayObject.render()
-  pygame.display.update()
-  clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    Screen.screen.fill("black")
+    DisplayObject.render()
+    Player.controls(player)
+    pygame.display.update()
+    clock.tick(60)
